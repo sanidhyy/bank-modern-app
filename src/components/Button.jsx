aa96b8115225460af6c6e0c73ea727947f85df67
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-09-16 19:26:13 +0530

    border fixed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
     >
       Get Started
     </button>
