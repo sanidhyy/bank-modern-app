34a2c6b0f4551456a7cb4361012869ee788c36fe
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-09-16 20:25:30 +0530

    Testimonials section done

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { feedback } from "../constants";
+import styles from "../styles";
+import FeedbackCard from "./FeedbackCard";
 
-const Testimonials = () => {
-  return <div>Testimonials</div>;
-};
+const Testimonials = () => (
+  <section
+    id="clients"
+    className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}
+  >
+    {/* Gradient */}
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
+
+    {/* Heading */}
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h2 className={`${styles.heading2} md:text-center`}>
+        What People are <br className="sm:block hidden" /> saying about us
+      </h2>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+    </div>
+
+    {/* Feedback Cards */}
+    <div
+      className="flex flex-wrap sm:justify-start w-full feedback-container relative z-[1]"
+      style={{ justifyContent: "center" }}
+    >
+      {feedback.map((card) => (
+        <FeedbackCard key={card.id} {...card} />
+      ))}
+    </div>
+  </section>
+);
 
 export default Testimonials;
