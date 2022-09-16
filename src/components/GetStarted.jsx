199fbf6e4595dd6417e0afb5c1cab3d834293fd8
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-09-16 21:23:07 +0530

    btn animation improved

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -6,7 +6,7 @@ const GetStarted = () => (
     className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
   >
     <div
-      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+      className={`${styles.flexCenter} flex-col bg-primary hover:bg-opacity-95 w-[100%] h-[100%] rounded-full`}
     >
       <div className={`${styles.flexStart} flex-row`}>
         <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
